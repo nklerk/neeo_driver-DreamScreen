402f0868a7e980b157a86fcfecd7dc9bd296fb9d
Niels de Klerk <Niels.dKlerk@vened.nl>
2018-03-01 16:20:37 +0100

    Added Comments.

--- uartCommCRC8.js
+++ uartCommCRC8.js
@@ -1,3 +1,14 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// This code is a work in progress test envirement to build a working NEEO driver.
+// At this point it is shared so others can read and copy my code if they like.
+// This code is far from being a complete driver.
+//
+// Niels de klerk
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// CRC8 Lookup table.
 let uartComm_crc8_table = [
     0x00, 0x07, 0x0E, 0x09, 0x1C, 0x1B, 0x12, 0x15, 0x38, 0x3F, 0x36, 0x31, 0x24, 0x23, 0x2A, 0x2D,
     0x70, 0x77, 0x7E, 0x79, 0x6C, 0x6B, 0x62, 0x65, 0x48, 0x4F, 0x46, 0x41, 0x54, 0x53, 0x5A, 0x5D,
@@ -17,6 +28,7 @@ let uartComm_crc8_table = [
     0xDE, 0xD9, 0xD0, 0xD7, 0xC2, 0xC5, 0xCC, 0xCB, 0xE6, 0xE1, 0xE8, 0xEF, 0xFA, 0xFD, 0xF4, 0xF3
 ]
 
+// Function to calculate crc8 checksum
 function uartComm_calculate_crc8(data) {
     size = (data[1] + 0x01);
     let cntr = 0x00;
